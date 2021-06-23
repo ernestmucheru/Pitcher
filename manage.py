162aeb017813e4aa5f046b562b3f4ef8323c8522
Your Name <you@example.com>
2021-06-24 00:57:43 +0300

    working initial app

--- manage.py
+++ manage.py
@@ -5,5 +5,9 @@ from flask_script import Manager
 app = create_app(os.getenv('FLASK_CONFIG')or 'default')
 manager = Manager(app)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app)
+
 if __name__ == '__main__':
     manager.run()
