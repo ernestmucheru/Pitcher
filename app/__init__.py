0e493020959651d442a9be38a81804276e0de2a1
Your Name <you@example.com>
2021-06-24 00:03:11 +0300

    updated flask compat

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
-
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     return app
