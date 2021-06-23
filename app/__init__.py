e4bb4498e1436e0bc190a358a39291d925feedc9
Your Name <you@example.com>
2021-06-24 01:08:08 +0300

    installed flask sqlachemy and initalized

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from config import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
 import os
-from app import create_app
+from app import create_app,db
 from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG')or 'default')
 manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
