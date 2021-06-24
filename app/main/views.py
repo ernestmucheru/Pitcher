20e9cf2afe78f15624b7939bda51608d62460647
Your Name <you@example.com>
2021-06-24 08:46:40 +0300

    created route to fetch posts from db

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,19 @@
 from flask import render_template
 from . import main
-from flask_login import login_required
+from flask_login import login_required,current_user
+from ..models import Pitch, User, Upvote
+import datetime
+from .. import db
 
 @main.route('/')
 def home():
-    return render_template('index.html')
+    def index():
+
+        interview_pitches = Pitch.get_pitches('interview')
+        product_pitches = Pitch.get_pitches('product')
+        promotion_pitches = Pitch.get_pitches('promotion')
+        
+        return render_template('index.html', interview=interview_pitches, product=product_pitches, promotion=promotion_pitches)
 
 @main.route('/about')
 def about():
