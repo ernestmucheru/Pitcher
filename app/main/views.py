8bb4eea5d4deffa02653a76507a3a6225ef2fc76
Your Name <you@example.com>
2021-06-24 08:55:43 +0300

    created route to add new pitch

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import main
 from flask_login import login_required,current_user
 from ..models import Pitch, User, Upvote
 import datetime
 from .. import db
+from .forms import PitchForm
 
 @main.route('/')
 def home():
@@ -15,6 +16,23 @@ def home():
         
         return render_template('index.html', interview=interview_pitches, product=product_pitches, promotion=promotion_pitches)
 
-@main.route('/about')
-def about():
-    return render_template('profile.html')
+@main.route('/pitch/new', methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    pitch_form=PitchForm()
+    if pitch_form.validate_on_submit():
+        title=pitch_form.title.data
+        pitch = pitch_form.text.data
+        category = pitch_form.category.data
+
+        #pitch instance
+        new_pitch = Pitch(pitch_title=title,pitch_content=pitch,category=category,user=current_user)
+
+        #save pitch method
+        new_pitch.save_pitch()
+        return redirect(url_for('main.index'))
+
+    title='New Pitch'
+    return render_template('new_pitch.html', title=title,pitch_form=pitch_form)
+
+
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
 from flask_login import UserMixin
 from . import login_manager
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -29,3 +30,27 @@ class User(db.Model,UserMixin):
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.String, nullable=False)
+    post = db.Column(db.String, nullable=False)
+    comment = db.relationship('Comment', backref='post', lazy='dynamic')
+    category = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    up_vote = db.relationship('Upvote', backref='post', lazy='dynamic')
+    down_vote = db.relationship('Downvote', backref='post', lazy='dynamic')
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"Post Title: {self.title}"
