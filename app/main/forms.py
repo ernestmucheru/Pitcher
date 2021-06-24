5e90022aea16d5d375b20a25ee2e1dc3bf951a98
Your Name <you@example.com>
2021-06-24 08:38:45 +0300

    added pitch forms

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    text = TextAreaField('Text', validators=[Required()])
+    category = SelectField('Category', choices=[('interview', 'Interview Pitch'), ('product', 'Product Pitch'), ('promotion', 'Promotion Pitch')],validators=[Required()])
+    submit = SubmitField('Post')
